b78307ff8c00176e4c74b88483536c6d40ff3cbf
Sahidannopal-15 <muhammadnaufalsahidan123@gmail.com>
2025-11-06 17:02:03 +0700

    menambahkan algoritma di seat.jsx

--- src/components/seat.jsx
+++ src/components/seat.jsx
@@ -1,11 +1,32 @@
 // Seat.jsx
 import { useState } from 'react'
 
+//Warna kursi sesuai status
 const seatColors = {
   kosong : 'bg-white',
-  selected : 'bg-green',
-  reserved : 'gray',
-  locked : 'red'
+  selected : 'bg-green-800',
+  reserved : 'bg-gray-800',
+  locked : 'bg-red-800'
 }
-
+//algoritma
+const Seat = (props) => {
+//menyimpan status kursi
+ const[seatStatus, setStatus] = useState(props.status)
+  const onSeatSelection = () =>{
+    props.onSeatClick()
+  }
+  if (seatStatus === 'kosong'){
+    setStatus('selected')
+  } else if (seatStatus === 'selected'){
+    setStatus('kosong')
+  }
+//Tampilan
+  return ( 
+    <div
+      onClick = {onSeatSelection}
+      className = {'w-8 h-8 border rounded cursor-pointer ${seatColors[seatStatus]} hover:scale-105'}
+      title = {`seat : ${seatStatus}`}
+    />
+  )
+}
 export default Seat
