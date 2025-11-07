6527c3be90b7aebe59f1731ba1de0bc5323c893e
Sahidannopal-15 <muhammadnaufalsahidan123@gmail.com>
2025-11-07 09:49:04 +0700

    menambahkan algoritma di Summary.jsx

--- src/components/summary.jsx
+++ src/components/summary.jsx
@@ -1,2 +1,22 @@
-import { props.kursiDipilih } from "./SeatLayout";
+import React from 'react';
 import{useState} from 'react';
+
+const Summary = ({kursiDipilih, hargaKursi}) => {
+const totalHarga = kursiDipilih.length * hargaKursi;
+
+return(
+    <div className="mt-8 text-center">
+      <h2 className="text-xl font-bold mb-2">Ringkasan Pemesanan</h2>
+      {kursiDipilih.length > 0 ? (
+        <>
+          <p>Kursi dipilih: {kursiDipilih.join(', ')}</p>
+          <p>Total harga: Rp {totalHarga.toLocaleString()}</p>
+        </>
+      ) : (
+        <p>Belum ada kursi yang dipilih</p>
+      )}
+    </div>
+);
+};
+
+export default Summary;
